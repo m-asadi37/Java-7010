4ca2e7bf4c03af700afc31dc3d29947f6c7b6441
m.asadi <masd9617@gmail.com>
2026-02-24 21:26:20 +0330

    session 5

--- code05/src/pack2/Main2.java
+++ code05/src/pack2/Main2.java
@@ -9,9 +9,13 @@ public class Main2 {
         System.out.println(test.getFinalId());
 
         test.setId(20);
-        test.setFinalId(24);
+//        test.setFinalId(24);
 
         System.out.println(test.getId());
         System.out.println(test.getFinalId());
+
+        Test test2 = new Test(1);
+        System.out.println(test2.getFinalId());
+        System.out.println(test2.getFinalId2());
     }
 }
--- code05/src/pack2/Test.java
+++ code05/src/pack2/Test.java
@@ -3,13 +3,21 @@ package pack2;
 //final : property - method - class
 public class Test {
 
+    //way 1 : initialize in ALL constructors
     public Test(int id, int finalId) {
         this.id = id;
         this.finalId = finalId;
     }
 
+    public Test(int id) {
+        this.id = id;
+        this.finalId = -id;
+    }
+
+    //way 2 : initialize in-line
     private int id;
     private final int finalId;
+    private final int finalId2 = 100;
 
     public int getId() {
         return id;
@@ -23,7 +31,11 @@ public class Test {
         return finalId;
     }
 
-//    public void setFinalId(int finalId) {
+    public int getFinalId2() {
+        return finalId2;
+    }
+
+    //    public void setFinalId(int finalId) {
 //        this.finalId = finalId;
 //    }
 }
